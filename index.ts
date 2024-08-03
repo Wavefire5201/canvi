4ac4a2085aad3eba591776f0eeac1c164b085505
chomp <outdoor10life@gmail.com>
2024-08-03 16:43:34 -0500

    tsx no make sense me

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { renderToReadableStream } from "react-dom/server.browser";
+import Pokemon from "./components/App";
+
+Bun.serve({
+  async fetch(request) {
+
+    const stream = await renderToReadableStream(<Pokemon />);
+
+    return new Response(stream, {
+      headers: { "Content-Type": "text/html" },
+    });
+  },
+});
+
+console.log("Listening ...");
+
